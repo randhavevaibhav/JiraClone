af031363b9169c6c150c0d7fcffb90e58f921742
randhavevaibhav <randhavevaibhav3@gmail.com>
2026-05-22 08:57:54 -0700

    fix(api): added vercel.json and seperated server files

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,22 +2,17 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import healthRoutes from "@/routes/health.routes"
 import { errorMiddleware } from "@/middlewares/erros.middleware";
-import { API_PORT } from "@/utils/constants";
-import { config } from "./utils/config";
 import swaggerUi from 'swagger-ui-express';
 import './docs/paths/health';
 import { generateOpenAPIDocument } from "@/swagger/swagger";
 
-
 const app = express();
-
 
 app.get("/",(_,res)=>{
     res.send({
         message:"Home page"
     })
 });
-
 
 app.use(
   '/docs',
@@ -35,7 +30,4 @@ app.use('/health', healthRoutes);
 //keep the error middleware last
 app.use(errorMiddleware);
 
-app.listen(API_PORT,()=>{
-console.log(`API started on PORT: ${API_PORT}`);
-console.log(`URL: ${config.API_URL}`)
-})
+export default app;
---
+++ apps/api/src/server.ts
@@ -0,0 +1,8 @@
+import app from '@/app';
+import { config } from '@/utils/config';
+import { API_PORT } from '@/utils/constants';
+
+app.listen(API_PORT,()=>{
+console.log(`API started on PORT: ${API_PORT}`);
+console.log(`URL: ${config.API_URL}`)
+})
